de74c6441c804bd74bb01bc2e30944d0cdfe1b13
May <may.johansson@hva.nl>
2019-03-15 10:26:40 +0100

    final commit, first week. Express nodejs server, styling needs some time

--- src/app.js
+++ src/app.js
@@ -11,12 +11,14 @@ app
   .set('views', 'views')
   .get('/', index)
   .get('/books', overviewBooks)
-  .get('/:frabl', detailBook)
   .get('/courses', overviewCourses)
+  .get('/books/:frabl', detailBook)
   .get('/courses/:frabl', detailCourse)
   .listen(3333)
 
 function index(req, res) {
+  console.log("index")
+
   fs.readFile('static/data/bookData.json', 'utf8', (err, books) => {
     if (err) {
       console.log('i fail')
@@ -26,7 +28,6 @@ function index(req, res) {
     try {
       console.log('i try')
       const data = JSON.parse(books)
-      console.log(data);
 
       res.render('main.ejs', {
         page: 0,
@@ -36,17 +37,34 @@ function index(req, res) {
       console.error(err)
     }
   })
-
 }
 
 function overviewBooks(req, res) {
+  console.log("overview books")
 
-  res.render('main.ejs', {
-    page: 0
+  fs.readFile('static/data/bookData.json', 'utf8', (err, books) => {
+    if (err) {
+      console.log('i fail')
+      console.error(err)
+      return
+    }
+    try {
+      console.log('i try')
+      const data = JSON.parse(books)
+
+      res.render('main.ejs', {
+        page: 0,
+        books: data
+      })
+    } catch (err) {
+      console.error(err)
+    }
   })
 }
 
 function overviewCourses(req, res) {
+  console.log("overview course")
+
   fs.readFile('static/data/courseData.json', 'utf8', (err, courses) => {
     if (err) {
       console.log('i fail')
@@ -58,26 +76,25 @@ function overviewCourses(req, res) {
       const data = JSON.parse(courses)
 
       res.render('main.ejs', {
-        page: 0,
+        page: 1,
         courses: data
       })
     } catch (err) {
       console.error(err)
     }
   })
+
 }
 
 function detailBook(req, res) {
   console.log("detail book")
-  const data = fs.readFile('static/data/bookData.json')
-  console.log(req.params)
 
-  res.render('book.ejs', {
-    page: 0,
-    book: data
-  })
+  const frabl = req.params.frabl
+
+
 }
 
 function detailCourse(req, res) {
+  console.log("detail course")
 
 }
